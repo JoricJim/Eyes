287b97799f545389571cac12d22135137aea898f
Никита <52201713+JoricJim@users.noreply.github.com>
2021-11-01 03:39:49 +0300

    Eyes Release

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const { TOKEN, CHANNEL_ID, SERVER_ID, YT_LINK } = require("./config.json");
-const { STATUS } = require("./status.json")
 const discord = require("discord.js");
 const client = new discord.Client();
 const ytdl = require('ytdl-core');
 
 client.on('ready', async () => {
   console.log("Started streaming " + (YT_LINK) + ` as ${client.user.tag}`);
-  client.user.setActivity((STATUS),{ type: 'STREAMING',
-  url: 'https://twitch.com/nocopyrightsounds'});
   let channel = client.channels.cache.get(CHANNEL_ID) || await client.channels.fetch(CHANNEL_ID)
 
   if(!channel) return;
